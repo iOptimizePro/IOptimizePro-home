341d6617312e9439861d340f7798277b8fe27fb4
pepedd864 <pepedd@qq.com>
2023-12-10 11:57:33 +0800

    feat: init template

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+useSeoMeta({
+  title: 'IOptimizePro',
+  description: '智能工业数据分析与优化平台',
+  // 搜索关键词
+  keywords: '工业数据分析 优化平台',
+})
+</script>
+
+<template>
+  <NuxtPage :keepalive="{ max: 10 }" />
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      // 解决403 Forbbiden问题
+      meta: [{ name: 'referrer', content: 'no-referrer' }],
+    },
+  },
+
+  modules: ['@nuxtjs/eslint-module'],
+})
